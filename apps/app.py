a0ddebf4fcedbc0cfe00ffd98d8afe73e24aae4b
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-05-26 09:54:52 +0900

    flask_sqlalchemyとflask_migrateの利用準備

--- apps/app.py
+++ apps/app.py
@@ -1,7 +1,23 @@
+from pathlib import Path
 from flask import Flask
+from flask_migrate import Migrate
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy()
 
 def create_app():
   app = Flask(__name__)
+
+  app.config.from_mapping(
+    SECRET_KEY="2AZSMss3p5QPbcY2hBsJ",
+    SQLARCHEMY_DATABASE_URI=
+      f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
+    SQLALCHEMY_TRACKMODIFICATIONS=False
+  )
+
+  db.init_app(app)
+  Migrate(app, db)
+
   from apps.crud import views as crud_views
   app.register_blueprint(crud_views.crud, url_prefix="/crud")
 
