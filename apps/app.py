acd675806d7bba3cc50af4201d68b01f9e9eb353
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-05-26 11:18:20 +0900

    新規作成画面のテンプレート作成 - create.html

--- apps/app.py
+++ apps/app.py
@@ -4,7 +4,7 @@ from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
 
-db = SQLAlchemy
+db = SQLAlchemy()
 csrf = CSRFProtect()
 
 def create_app():
