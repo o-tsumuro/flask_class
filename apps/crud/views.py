d637100503a94e433ed12410fb1d645ad3219dee
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-05-26 10:46:58 +0900

    create_userエンドポイントの作成

--- apps/crud/views.py
+++ apps/crud/views.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, render_template
+from apps.crud.forms import UserForm
+from apps.app import db
+from apps.crud.models import User
+from flask import Blueprint, render_template, redirect, url_for
 
 crud = Blueprint(
   "crud",
@@ -10,3 +13,22 @@ crud = Blueprint(
 @crud.route("/")
 def index():
   return render_template("crud/index.html")
+
+@crud.route("/user/new", methods=["GET", "POST"])
+def create_user():
+  # UserFormをインスタンス化する
+  form = UserForm()
+  # フォームの値をバリデートする
+  if form.validate_on_submit():
+    # ユーザーを作成する
+    user = User(
+      username=form.username.data,
+      email=form.email.data,
+      password=form.password.data,
+    )
+    # ユーザーを追加してコミットする
+    db.session.add(user)
+    db.session.commit()
+    # ユーザーの一覧画面へリダイレクトする
+    return redirect(url_for("crud.users"))
+  return render_template("crud/create.html", form=form)
