fc546bdbcf885c11d9c29298c733d27db32e0d5f
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-05-30 10:40:01 +0900

    Userモデルの定義を更新

--- apps/crud/models.py
+++ apps/crud/models.py
@@ -1,10 +1,11 @@
 from datetime import datetime
 
-from apps.app import db
-from werkzeug.security import generate_password_hash
+from apps.app import db, login_manager
+from flask_login import UserMixin
+from werkzeug.security import generate_password_hash, check_password_hash
 
 # db.Model を継承したUserクラスを作成する
-class User(db.Model):
+class User(db.Model, UserMixin):
   # テーブル名を指定する
   __tablename__ = "users"
   # カラムを定義する
@@ -26,3 +27,15 @@ class User(db.Model):
   @password.setter
   def password(self, password):
     self.password_hash = generate_password_hash(password)
+
+  # パスワードをチェックする
+  def verify_password(self, password):
+    return check_password_hash(self.password_hash, password)
+  
+  # メールアドレス重複チェックをする
+  def is_duplicate_email(self):
+    return User.query.filter_by(email=self.email).first() is not None
+  
+@login_manager.user_loader
+def load_user(user_id):
+  return User.query.get(user_id)
