1c338a1eca356d14877ce4a31f733ee2c23c69c1
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-06-25 10:46:04 +0900

    dbのmigrateとupgrade

--- apps/detector/models.py
+++ apps/detector/models.py
@@ -4,7 +4,7 @@ from apps.app import db
 class UserImage(db.Model):
   __tablename__ = "user_images"
   id = db.Column(db.Integer, primary_key=True)
-  user_id = db.Column(db.String, db.ForeignKey("user.id"))
+  user_id = db.Column(db.String, db.ForeignKey("users.id"))
   image_path = db.Column(db.String)
   is_detected = db.Column(db.Boolean, default=False)
   created_at = db.Column(db.DateTime, default=datetime.now)
