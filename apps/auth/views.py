cd9ae409c1e9f77b4296901c7f351b69f5c5cf30
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-06-26 09:53:55 +0900

    サインアップ画面のエンドポイント

--- apps/auth/views.py
+++ apps/auth/views.py
@@ -34,7 +34,7 @@ def signup():
     login_user(user)
     next_ = request.args.get("next")
     if next_ is None or not next_.startswith("/"):
-      next_ = url_for("crud.users")
+      next_ = url_for("detector.index")
     return redirect(next_)
   
   return render_template("auth/signup.html", form=form)
