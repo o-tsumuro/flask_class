47e3d900f40b5f32969d695d3c40efd01876cdba
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-07-01 12:23:19 +0900

    exec_detect関数

--- apps/detector/views.py
+++ apps/detector/views.py
@@ -1,3 +1,11 @@
+import random
+import uuid
+
+import cv2
+import numpy as np
+import torch
+import torchvision
+
 import uuid
 from pathlib import Path
 from apps.app import db
@@ -12,7 +20,9 @@ from flask import (
   redirect,
   url_for,
 )
+
 from flask_login import current_user, login_required
+from PIL import Image
 
 dt = Blueprint("detector", __name__, template_folder="templates")
 
@@ -54,3 +64,72 @@ def upload_image():
 
     return redirect(url_for("detector.index"))
   return render_template("detector/upload.html", form=form)
+
+def make_color(labels):
+  colors = [[random.randint(0, 255) for _ in range(3)] for _ in labels]
+  color = random.choice(colors)
+  return color
+
+def make_line(result_image):
+  line = round(0.003 * max(result_image.shape[0:2])) + 1
+  return line
+
+def draw_lines(c1, c2, result_image, line, color):
+  cv2.rectangle(result_image, c1, c2, color, thickness=line)
+  return cv2
+
+def draw_texts(result_image, line, c1, cv2, color, labels, label):
+  display_txt = f"{labels[label]}"
+  font = max(line - 1, 1)
+  t_size = cv2.getTextSize(
+    display_txt, 0, fontScale=line / 3, thickness=font
+  )[0]
+  c2 = c1[0] + t_size[0], c1[1] - t_size[1] - 3
+  cv2.rectangle(result_image, c1, c2, color, -1)
+  cv2.putText(
+    result_image,
+    display_txt,
+    (c1[0], c1[1] - 2),
+    0,
+    line / 3,
+    [225, 225, 225],
+    thickness=font,
+    lineType=cv2.LINE_AA,
+  )
+  return cv2
+
+def exec_detector(target_image_path):
+  labels = current_app.config["LABELS"]
+  image = Image.open(target_image_path)
+  image_tensor = torchvision.transform.functional.to_tensor(image)
+
+  model = torch.load(Path(current_app.root_path, "detector", "model.pt"))
+  model = model.eval()
+  output = model([image_tensor])[0]
+
+  tags = []
+  result_image = np.array(image.copy())
+  for box, label, score in zip(
+    output["boxes"], output["labels"], output["scores"]
+  ):
+    if score > 0.5 and labels[label] not in tags:
+      color = make_color(labels)
+      line = make_line(result_image)
+      c1 = (int(box[0]), int(box[1]))
+      c2 = (int(box[2]), int(box[3]))
+      cv2 = draw_lines(c1, c2, result_image, line, color)
+      cv2 = draw_texts(result_image, line, c1, cv2, color, labels, label)
+      tags.append(labels[label])
+
+  detected_image_file_name = str(uuid.uuid4()) + ".jpg"
+  detected_image_file_path = str(
+    Path(current_app.config["UPLOAD_FOLDER"],
+         detected_image_file_name)
+  )
+
+  cv2.imwrite(
+    detected_image_file_path, cv2.cvtColor(
+      result_image, cv2.COLOR_RGB2BGR)
+  )
+
+  return tags, detected_image_file_name
