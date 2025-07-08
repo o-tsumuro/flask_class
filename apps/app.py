36ed19ca02baac9c9e07e9999938f719dc580732
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-07-08 14:09:59 +0900

    カスタムエラー画面のエンドポイントの作成

--- apps/app.py
+++ apps/app.py
@@ -1,5 +1,5 @@
 from pathlib import Path
-from flask import Flask
+from flask import Flask, render_template
 from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
@@ -31,6 +31,9 @@ def create_app(config_key):
   Migrate(app, db)
   login_manager.init_app(app)
 
+  app.register_error_handler(404, page_not_found)
+  app.register_error_handler(500, internal_server_error)
+
   from apps.crud import views as crud_views
   app.register_blueprint(crud_views.crud, url_prefix="/crud")
 
@@ -42,3 +45,8 @@ def create_app(config_key):
 
   return app
 
+def page_not_found(e):
+  return render_template("404.html"), 404
+
+def internal_server_error(e):
+  return render_template("500.html"), 500
