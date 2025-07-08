6dc92e840e19d3a17a7919b104800ec2ce332164
o-tsumuro <fko2447009@stu.o-hara.ac.jp>
2025-07-08 15:05:59 +0900

    ログイン時のテストの実装

--- tests/detector/test_views.py
+++ tests/detector/test_views.py
@@ -2,3 +2,15 @@ def test_index(client):
   rv = client.get("/")
   assert "ログイン" in rv.data.decode()
   assert "画像新規登録" in rv.data.decode()
+
+def signup(client, username, email, password):
+  data = dict(userame=username, email=email, password=password)
+  return client.post("/auth/signup", data=data, follow_redirect=True)
+
+def test_index_signup(client):
+  rv = signup(client, "admin", "flaskbook@example.com", "password")
+  assert "admin" in rv.data.decode()
+
+  rv = client.get("/")
+  assert "ログアウト" in rv.data.decode()
+  assert "画像新規登録" in rv.data.decode()
